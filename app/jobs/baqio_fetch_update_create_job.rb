f7ebbb6df146cf4bbc3542932c4f325492adf973
Guillaume <groche@ekylibre.com>
2021-02-24 09:37:55 +0100

    Added ItemSales

--- app/jobs/baqio_fetch_update_create_job.rb
+++ app/jobs/baqio_fetch_update_create_job.rb
@@ -2,6 +2,35 @@ class BaqioFetchUpdateCreateJob < ActiveJob::Base
   queue_as :default
   include Rails.application.routes.url_helpers
 
+  SALE_STATE = [
+    { order_state: "draft", sale_state: :draft},
+    { order_state: "pending", sale_state: :estimate},
+    { order_state: "validated", sale_state: :order},
+    { order_state: "removed", sale_state: :aborted},
+    { order_state: "invoiced", sale_state: :invoice},
+    { order_state: "cancelled", sale_state: :refused}
+  ].freeze
+
+  PRODUCT_CATEGORY_BAQIO = [
+    { value: 1, category: "Vin tranquille" },
+    { value: 2, category: "Vin mousseux" },
+    { value: 3, category: "Cidre" },
+    { value: 4, category: "VDN et VDL AOP" },
+    { value: 5, category: "Bière" },
+    { value: 6, category: "Boisson fermentée autre que le vin et la bière" },
+    { value: 7, category: "Rhum des DOM" },
+    { value: 8, category: "Autre produit intermédiaire que VDN et VDL AOP" },
+    { value: 9, category: "Autre" },
+    { value: 10, category: "Pétillant de raisin" },
+    { value: 11, category: "Poiré" },
+    { value: 12, category: "Hydromel" },
+    { value: 13, category: "Alcool (autre que Rhum)" },
+    { value: 14, category: "Pétillant de raisin" },
+    { value: 15, category: "Rhums tiers (hors DOM) et autres rhums" },
+    { value: 16, category: "Matière première pour alcool non alimentaire" },
+    { value: 17, category: "Matière première pour spiritueux" }
+  ].freeze
+
   def perform
     # Page need to be set-up to fetch orders from different Baqio pages
     @page = 0
@@ -10,7 +39,6 @@ class BaqioFetchUpdateCreateJob < ActiveJob::Base
     @page_with_orders = []
 
     begin
-
       # Create ProductNatureCategory and ProductNature from Baqio product_families
       Baqio::BaqioIntegration.fetch_family_product.execute do |c|
         c.success do |list|
@@ -41,26 +69,26 @@ class BaqioFetchUpdateCreateJob < ActiveJob::Base
               )
             end
 
-            pn = ProductNature.find_by(reference_name: "wine") || ProductNature.import_from_nomenclature("wine")
+            # pn = ProductNature.find_by(reference_name: "wine") || ProductNature.import_from_nomenclature("wine")
 
-            pns = ProductNature.where("provider ->> 'id' = ?", family_product[:id].to_s)
-            if pns.any?
-              pns.first
-            else
-              new_pn = ProductNature.create!(
-                name: family_product[:name],
-                variety: pn.variety,
-                derivative_of: pn.derivative_of,
-                reference_name: pn.reference_name,
-                active: pn.active,
-                evolvable: pn.evolvable,
-                population_counting: pn.population_counting,
-                variable_indicators_list: [:certification, :reference_year, :temperature],
-                frozen_indicators_list: pn.frozen_indicators_list,
-                type: pn.type,
-                provider: {vendor: "Baqio", name: "Baqio_product_family", id: family_product[:id]}
-              )
-            end
+            # pns = ProductNature.where("provider ->> 'id' = ?", family_product[:id].to_s)
+            # if pns.any?
+            #   pns.first
+            # else
+            #   new_pn = ProductNature.create!(
+            #     name: family_product[:name],
+            #     variety: pn.variety,
+            #     derivative_of: pn.derivative_of,
+            #     reference_name: pn.reference_name,
+            #     active: pn.active,
+            #     evolvable: pn.evolvable,
+            #     population_counting: pn.population_counting,
+            #     variable_indicators_list: [:certification, :reference_year, :temperature],
+            #     frozen_indicators_list: pn.frozen_indicators_list,
+            #     type: pn.type,
+            #     provider: {vendor: "Baqio", name: "Baqio_product_family", id: family_product[:id]}
+            #   )
+            # end
           end
         end
       end
@@ -126,41 +154,71 @@ class BaqioFetchUpdateCreateJob < ActiveJob::Base
             end
 
             # Create or Find Sale
-            # order[:amount] = amount
-            # order[:fulfillment_status] = status
-            # order[:id] = provider id
-            # order[:order_lines_not_deleted] = items
             sales = Sale.where("provider ->> 'id' = ?", order[:id].to_s)
             if sales.any?
               sale = sales.first
             else
-              if order[:id] == 133607
+              if order[:id] == 133607 # || order[:id] == 133605
                 # Create Variants from Baqio order_lines
+                invoiced_date = order[:state] == "invoiced" ? order[:date] : nil
+                confirmed_date = order[:state] == "validated" ? order[:date] : nil
 
-                binding.pry
                 sale = Sale.create!(
                   client_id: entity.id,
-                  provider: {vendor: "Baqio", name: "Baqio_order", id: order[:id]}
+                  provider: {vendor: "Baqio", name: "Baqio_order", id: order[:id]},
+                  invoiced_at: invoiced_date,
+                  confirmed_at: confirmed_date
                 )
 
+                sale.update!(state: sale_state_matching(order[:state]))
+                sale.affair.refresh!
+
+                # Find Tax Lines
+                @tax_order = Tax.find_by(amount: order[:tax_lines].first[:tax_percentage])
+
+                # Create SaleItem
                 order[:order_lines_not_deleted].each do |product_order|
 
                   product_nature_variants = ProductNatureVariant.where("provider ->> 'id' = ?", product_order[:id].to_s)
 
+                  binding.pry
                   if product_nature_variants.any?
                     product_nature_variant = product_nature_variants.first
                   else
-                    # Find Baqio product_family_id
+                    binding.pry
+
+                    # Find Baqio product_family_id and product_category_id
                     Baqio::BaqioIntegration.fetch_product_variants(product_order[:product_variant_id]).execute do |c|
                       c.success do |order|
-                        @product_nature_and_category_id = order["product"]["product_family_id"].to_s
+                        @product_category_id = order["product"]["product_family_id"].to_s
+                        @product_nature_id = order["product"]["product_category_id"].to_s
                       end
                     end
 
-                    product_nature = ProductNature.find_by("provider ->> 'id' = ?", @product_nature_and_category_id)
-                    product_nature_category = ProductNatureCategory.find_by("provider ->> 'id' = ?", @product_nature_and_category_id)
+                    # Find or Create ProductNature from Baqio Product Type
+                    pns = ProductNature.where("provider ->> 'id' = ?", @product_nature_id)
+                    if pns.any?
+                      product_nature = pns.first
+                    else
+                      pn = ProductNature.find_by(reference_name: "wine") || ProductNature.import_from_nomenclature("wine")
 
-                    binding.pry
+                      product_nature = ProductNature.create!(
+                        name: product_category_baqio_matching(@product_nature_id.to_i),
+                        variety: pn.variety,
+                        derivative_of: pn.derivative_of,
+                        reference_name: pn.reference_name,
+                        active: pn.active,
+                        evolvable: pn.evolvable,
+                        population_counting: pn.population_counting,
+                        variable_indicators_list: [:certification, :reference_year, :temperature],
+                        frozen_indicators_list: pn.frozen_indicators_list,
+                        type: pn.type,
+                        provider: {vendor: "Baqio", name: "Baqio_product_type", id: @product_nature_id}
+                      )
+                    end
+
+                    product_nature_category = ProductNatureCategory.find_by("provider ->> 'id' = ?", @product_category_id)
+
                     # Find or create new variant
                     product_nature_variant =  ProductNatureVariant.create!(
                       category_id: product_nature_category.id,
@@ -170,29 +228,18 @@ class BaqioFetchUpdateCreateJob < ActiveJob::Base
                       provider: {vendor: "Baqio", name: "Baqio_product_order", id: product_order[:id]}
                     )
 
-                    binding.pry
-
                     sale_item = SaleItem.create!(
                       sale_id: sale.id,
                       variant_id: product_nature_variant.id,
                       label: "#{product_order[:name]} - #{product_order[:complement]} - #{product_order[:description]}",
                       currency: product_order[:price_currency],
-                      amount: product_order[:quantity].to_i,
-                      unit_amount: product_order[:price_cents],
-                      pretax_amount: product_order[:total_price_with_tax_cents],
-                      unit_pretax_amount: product_order[:total_price_with_tax_cents] / product_order[:quantity].to_i
+                      quantity: product_order[:quantity].to_d,
+                      unit_pretax_amount: (product_order[:price_cents] / 100.0).to_d,
+                      tax_id: @tax_order.id
                     )
                   end
-
-
                 end
 
-
-                binding.pry
-                # sale = Sale.create!(
-                #   client_id: entity.id,
-                #   provider: {vendor: "Baqio", name: "Baqio_order", id: order[:id]}
-                # )
               end
             end
 
@@ -209,6 +256,13 @@ class BaqioFetchUpdateCreateJob < ActiveJob::Base
 
   private
 
+  def product_category_baqio_matching(category_id)
+    PRODUCT_CATEGORY_BAQIO.find {|h| h[:value] == category_id}[:category]
+  end
+
+  def sale_state_matching(order_state)
+    SALE_STATE.find {|h| h[:order_state] == order_state}[:sale_state]
+  end
 
   def build_address_cz(city, zip)
     return nil if city.blank? && zip.blank?
