7cd0a36166d8ca5f65cc8db6bede93346949eb49
Guillaume <groche@ekylibre.com>
2021-06-10 12:17:11 +0200

    Add new field to select good url to connect to baqio

--- app/integrations/baqio/baqio_integration.rb
+++ app/integrations/baqio/baqio_integration.rb
@@ -5,15 +5,15 @@ module Baqio
 
   class BaqioIntegration < ActionIntegration::Base
     # Set url needed for Baqio API v2
-    BASE_URL = 'https://demo.baqio.com/api/v1'.freeze
-    FAMILY_URL = BASE_URL + '/product_families'
-    ORDERS_URL = BASE_URL + '/orders'
-    VARIANTS_URL = BASE_URL + '/product_variants'
-    PAYMENT_SOURCES_URL = BASE_URL + '/payment_sources'
-    BANK_INFORMATIONS_URL = BASE_URL + '/bank_informations'
-    COUNTRY_TAXES_URL = BASE_URL + '/country_taxes'
+    FAMILY_URL = '/product_families'
+    ORDERS_URL = '/orders'
+    VARIANTS_URL = '/product_variants'
+    PAYMENT_SOURCES_URL = '/payment_sources'
+    BANK_INFORMATIONS_URL = '/bank_informations'
+    COUNTRY_TAXES_URL = '/country_taxes'
 
     authenticate_with :check do
+      parameter :url
       parameter :api_key
       parameter :api_password
       parameter :api_secret
@@ -21,6 +21,11 @@ module Baqio
 
     calls :authentication_header, :fetch_payment_sources, :fetch_bank_informations, :fetch_family_product, :fetch_orders,
           :fetch_product_variants, :fetch_country_taxes
+
+    def base_url
+      integration = fetch integration
+      "https://#{integration.parameters['url']}/api/v1".freeze
+    end
 
     # Build authentication header with api_key and password parameters
     # DOC https://api-doc.baqio.com/docs/api-doc/Baqio-Public-API.v1.json
@@ -34,7 +39,7 @@ module Baqio
     # https://api-doc.baqio.com/docs/api-doc/Baqio-Public-API.v1.json/paths/~1payment_sources/get
     def fetch_payment_sources
       # Call API
-      get_json(PAYMENT_SOURCES_URL, authentication_header) do |r|
+      get_json(base_url + PAYMENT_SOURCES_URL, authentication_header) do |r|
         r.success do
           list = JSON(r.body).map(&:deep_symbolize_keys)
         end
@@ -44,7 +49,7 @@ module Baqio
     # https://api-doc.baqio.com/docs/api-doc/Baqio-Public-API.v1.json/components/schemas/BankInformation
     def fetch_bank_informations
       # Call API
-      get_json(BANK_INFORMATIONS_URL, authentication_header) do |r|
+      get_json(base_url + BANK_INFORMATIONS_URL, authentication_header) do |r|
         r.success do
           list = JSON(r.body).map(&:deep_symbolize_keys)
         end
@@ -54,7 +59,7 @@ module Baqio
     # GET recupérer la liste des familles de produits - OK
     def fetch_family_product
       # Call API
-      get_json(FAMILY_URL, authentication_header) do |r|
+      get_json(base_url + FAMILY_URL, authentication_header) do |r|
         r.success do
           list = JSON(r.body).map(&:deep_symbolize_keys)
         end
@@ -64,7 +69,7 @@ module Baqio
     # https://api-doc.baqio.com/docs/api-doc/Baqio-Public-API.v1.json/paths/~1orders/get
     def fetch_orders(page)
       # Call API
-      get_json(ORDERS_URL + "?page=#{page}", authentication_header) do |r|
+      get_json(base_url + ORDERS_URL + "?page=#{page}", authentication_header) do |r|
         r.success do
           list = JSON(r.body).map(&:deep_symbolize_keys)
         end
@@ -72,7 +77,7 @@ module Baqio
     end
 
     def fetch_product_variants(product_variant_id)
-      get_json(VARIANTS_URL + "/#{product_variant_id}", authentication_header) do |r|
+      get_json(base_url + VARIANTS_URL + "/#{product_variant_id}", authentication_header) do |r|
         r.success do
           list = JSON(r.body).deep_symbolize_keys
         end
@@ -81,7 +86,7 @@ module Baqio
 
     def fetch_country_taxes
       # Call API
-      get_json(COUNTRY_TAXES_URL, authentication_header) do |r|
+      get_json(base_url + COUNTRY_TAXES_URL, authentication_header) do |r|
         r.success do
           list = JSON(r.body).map(&:deep_symbolize_keys)
         end
@@ -98,7 +103,8 @@ module Baqio
       string_to_encode = "#{integration.parameters['api_key']}:#{integration.parameters['api_password']}"
       auth_encode = Base64.encode64(string_to_encode).delete("\n")
       header = { authorization: "Basic #{auth_encode}", content_type: :json, accept: :json }
-      get_json(FAMILY_URL, header) do |r|
+      base_url = "https://#{integration.parameters['url']}/api/v1".freeze
+      get_json(base_url + FAMILY_URL, header) do |r|
         if r.state == :success
           puts 'check success'.inspect.green
           Rails.logger.info 'CHECKED'.green
