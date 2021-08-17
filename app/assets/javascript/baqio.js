00b4950e233b695f6932db06ab14618a5f543e74
Guillaume Roche <groche@ekylibre.com>
2021-08-17 08:09:09 +0000

    Debug/baqio selector

--- app/assets/javascript/baqio.js
+++ app/assets/javascript/baqio.js
@@ -1,9 +1,10 @@
 (function (E) {
-    function handleSelectBaqioUrl() {
-      const element = document.querySelector('#integration_parameters_baqio_web_address')
-        
-      if (element) {
+    function handleSelectBaqioUrl() {        
+      if (document.querySelector("#integration_nature[value='baqio']") != undefined) {
+        const element = document.querySelector('#integration_parameters_url')
         let new_element = document.createElement('select')
+        new_element.setAttribute("id", "integration_parameters_url")
+        new_element.setAttribute("name", "integration[parameters][url]")
         new_element.innerHTML = '<option value="demo.baqio.com">demo.baqio.com</option>' + '<option value="app.baqio.com">app.baqio.com</option>'
 
         element.replaceWith(new_element);
