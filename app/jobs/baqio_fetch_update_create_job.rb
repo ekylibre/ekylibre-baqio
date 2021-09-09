ec32bc1a0c8dbf74076c8ae47e70569e5e134adf
Manuel <mlabous@ekylibre.com>
2021-09-09 09:05:38 +0000

    Feature/synchronisation and notifications

--- app/jobs/baqio_fetch_update_create_job.rb
+++ app/jobs/baqio_fetch_update_create_job.rb
@@ -6,7 +6,8 @@ class BaqioFetchUpdateCreateJob < ActiveJob::Base
 
   VENDOR = 'baqio'
 
-  def perform
+  def perform(user_id: nil)
+    Preference.set!(:baqio_fetch_job_running, true, :boolean)
     begin
       # Create ProductNatureCategory and ProductNature from Baqio product_families
       pnc_handler = Integrations::Baqio::Handlers::ProductNatureCategories.new(vendor: VENDOR)
@@ -25,21 +26,39 @@ class BaqioFetchUpdateCreateJob < ActiveJob::Base
     rescue StandardError => error
       Rails.logger.error $ERROR_INFO
       Rails.logger.error $ERROR_INFO.backtrace.join("\n")
+      @error = error
       ExceptionNotifier.notify_exception($ERROR_INFO, data: { message: error })
     end
+    if (user = User.find_by_id(user_id))
+      notif_params =  if @error.nil?
+                        correct_baqio_fetch_params
+                      else
+                        errors_baqio_fetch_params
+                      end
+      locale = user.language.present? ? user.language.to_sym : :eng
+      I18n.with_locale(locale) do
+        user.notifications.create!(notif_params)
+      end
+    end
+    Preference.set!(:baqio_fetch_job_running, false, :boolean)
   end
 
   private
 
-    def error_notification_params(error)
+    def errors_baqio_fetch_params
       {
-        message: 'error_during_baqio_api_call',
+        message: :failed_baqio_fetch_params.tl,
         level: :error,
-        target_type: '',
-        target_url: '',
-        interpolations: {
-          error_message: error
-        }
+        interpolations: {}
+      }
+    end
+
+    def correct_baqio_fetch_params
+      {
+        message: :correct_baqio_fetch_params.tl,
+        level: :success,
+        target_url: '/backend/sales',
+        interpolations: {}
       }
     end
 end
